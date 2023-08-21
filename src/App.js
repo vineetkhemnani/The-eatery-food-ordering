4ef90b0bed277176ff7b586cd29d9bb1bf7b70ee
vineetkhemnani <vineetkhemnani09@gmail.com>
2023-08-21 11:30:17 +0530

    few fixes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Contact from './components/Contact';
 import RestaurantMenu from './components/RestaurantMenu';
 import Profile from './components/Profile';
 import Shimmer from './components/Shimmer';
-import UserContext from './utils/userContext';
+import UserContext from './utils/userContext.js';
 import { Provider } from 'react-redux';
 import store from './utils/store';
 import Cart from './components/Cart.js'
